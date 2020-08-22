0f4455ac524e6e6a2c1708e636598518f2931989
odmishien <tmtm.yagi.007@gmail.com>
2020-08-22 14:15:20 +0900

    入室時にroomIDを割り振ってみる

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var app = require('express')();
-var server= require('http').Server(app);
-var io = require('socket.io')(server);
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
+let rooms = ["room1", "room2"];
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    let message = JSON.parse(msg);
+    console.log(message);
+    socket.join(message.room);
+    io.to(message.room).emit("chat message", message.content);
+  });
+  socket.once("join room", () => {
+    let room = rooms[Math.floor(Math.random() * 2)];
+    console.log(room);
+    io.to(socket.id).emit("room notification", room);
   });
 });
 
-server.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(3000, () => {
+  console.log("listening on :3000");
 });
